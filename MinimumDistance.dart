e6420412cda9255d0872b8a6b85253f582126dc4
sadaf walliyani <sadafwalliyani@gmail.com>
2022-04-03 13:55:55 +0500

    task compete

--- MinimumDistance.dart
+++ MinimumDistance.dart
@@ -1,12 +1,26 @@
 
 
+// void main(){
+// var arr1=[7 ,9 ,5 ,11 ,7 ,4 ,12 ,6 ,2 ,11];
+// var result;
+  
+  
+//   for (int i = 1; i <= 100; i++) {
+//     if (arr1.contains(i)==false) {
+//       result=i;
+//      }
+//   } print(result);
+// }
+
+
 void main(){
-List array=[7 ,9 ,5 ,11 ,7 ,4 ,12 ,6 ,2 ,11];
-
-for(int i=1; i <= 12; i++){
-if (array.contains(i)==false){
-
-}
-} print(i);
-
-}
+ 
+List array=[7 ,9];
+   int n = array.length;
+  int x = 0;
+  int y = 6;
+  
+  print("Minimum distance between " + x + " and " + y + " is "+ min.minDist(array, n, x, y));
+    
+    
+   }
--- Pyramid.dart
+++ Pyramid.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
  
 void main(){
-    var n = 5;
+    var n = 9;
     
           
     for(var i=1; i<=n; i++) {
-        for(var j=0; j<i; j++) {
+        for(var j=0; j<i; j++) 
+        {
             stdout.write('$i');
         }
         print('');
