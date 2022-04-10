1025f4c31807c583d3741b7279d0b3aca4ce2d18
sadaf walliyani <sadafwalliyani@gmail.com>
2022-04-10 20:55:56 +0500

    Task Complete

--- MarksheetWithMapFunction.dart
+++ MarksheetWithMapFunction.dart
@@ -1,30 +1,31 @@
+
+
 void main(){
-Map names={'sadaf':{50+60+70,},
-           'Madiha':{60+40+80,},
-           'Samra':{30+50+90},
-           'Zara':{90+80+70},
-           'Sara':{85+55+70}};
-int sum=();      
-for (var i=1; i<=sum.length; i++)
-{ 
 
-  print(('Percentage:${(sum*100)~/300}'));
-
+Map<String, int> student = {
+    "Sadaf": 40,
+    "English": 60,
+    "Maths": 70,
+    "Science": 90,        
+  };
+  var values = student.values;
+var result = values.reduce((sum, element) => sum + element);
+percentage(student.keys.first,result);
+  
+  
 }
+percentage(student,result){
+  print('Names of student:$student');
+var obt=result;
+    var percentage=(obt*100)~/300;
+  print("Per%: $percentage");
+    if (percentage<=90){
+    print("Grade: A+");
+  }
+    else if (percentage<=80){
+    print("Grade:A");}
+      else if (percentage<=70){
+    print("Grade:B");}
 
-} 
-// percentagesfunction(names,sum){
-//   print("Name of Student: $names");
-//   var obt=sum;
-//   var percentage=(obt*100)~/300;
-//   print("Per%: $percentage");
-//     if (percentage<=90){
-//     print("Grade: A+");
-//   }
-//     else if (percentage<=80){
-//     print("Grade:A");}
-//       else if (percentage<=70){
-//     print("Grade:B");}
-
-//     else (print("Grade: C"));
-//   print("++++++++++++++++++++++++++++++++++++");}
+    else (print("Grade: C"));
+  print("++++++++++++++++++++++++++++++++++++");} 
